d24187a70eca7afe7eb2dfcbddaa056e703f74ae
Santi <121914283+rpssanti@users.noreply.github.com>
2023-01-08 20:32:30 +0100

    Add files via upload

---
+++ ejercicio crear lista 2.js
@@ -0,0 +1,25 @@
+const name = "santy"
+const edad = 32
+const nacimiento = new Date ("2 october 2020")
+const desarrollador = false
+const libro =  {
+    name :"Mano negra",
+    autor : "Frederick Person",
+    fecha : new Date ("2 October 2002")
+    
+}
+
+
+
+
+
+
+
+
+
+const lista = [name,edad,nacimiento,desarrollador,libro];
+
+console.log(name)
+console.log(nacimiento)
+console.log(desarrollador)
+console.log(libro)
